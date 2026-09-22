81ac90eb99571416389461942cfe0b093609cb5d
FeliciaJMartell <felicia.z.johansson@gmail.com>
2026-09-22 19:56:11 +0200

    klar med javascript, påbörja kommentarer

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ const valkomstText = document.getElementById("valkomstText");
 const logoutKnapp = document.getElementById("logoutKnapp");
 const loginRubrik = document.getElementById("loginRubrik");
 const svampBild = document.querySelector(".svamp");
+const sparadAnvandare = localStorage.getItem("inloggadAnvandare");
+
+if (sparadAnvandare) {
+    visaInloggadVy(sparadAnvandare);
+}
 
 loginForm.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -20,16 +25,38 @@ loginForm.addEventListener("submit", function (event) {
     console.log('losenord:', inskrivetLosenord);
 
     if (inskrivetNamn == namn && inskrivetLosenord == losenord) {
-        valkomstText.textContent = "Välkommen " + inskrivetNamn + ", du är nu inloggad";
-
-        loginForm.classList.add("hidden");
-        svampBild.classList.add("hidden");
-        loginRubrik.classList.add("hidden");
-        inloggadVy.classList.remove("hidden");
+        visaInloggadVy(inskrivetNamn);
+        localStorage.setItem("inloggadAnvandare", inskrivetNamn);
 
     } else {
         felMeddelande.textContent = "Felaktiga inloggningsuppgifter";
 
     }
+});
+
+function visaInloggadVy(inskrivetNamn) {
+    valkomstText.textContent = "Välkommen " + inskrivetNamn + ", du är nu inloggad.";
+
+    loginForm.classList.add("hidden");
+    svampBild.classList.add("hidden");
+    loginRubrik.classList.add("hidden");
+    inloggadVy.classList.remove("hidden");
+}
+
+logoutKnapp.addEventListener("click", () => {
+    inloggadVy.classList.add("hidden");
+
+    loginForm.classList.remove("hidden");
+    svampBild.classList.remove("hidden");
+    loginRubrik.classList.remove("hidden");
+
+    document.getElementById("namn").value = "";
+    document.getElementById("losenord").value = "";
+    felMeddelande.textContent = "";
+
+    localStorage.removeItem("inloggadAnvandare");
 
 });
+
+
+
